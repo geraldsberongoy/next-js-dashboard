64692afda01a81fc12d1d8cf7a866623917c26e3
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-11-03 22:50:26 +0800

    fix(metadata): standardize string quotes in invoice edit and create pages

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,11 +1,11 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Edit Invoice - Acme Dashboard',
+  title: "Edit Invoice - Acme Dashboard",
 };
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,10 +1,10 @@
 import Form from "@/app/ui/invoices/create-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers } from "@/app/lib/data";
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Create Invoice - Acme Dashboard',
+  title: "Create Invoice - Acme Dashboard",
 };
 
 export default async function Page() {
