834af41b74a5164b137b73d1d4de0bc08d7fe4a1
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-11-03 22:20:35 +0800

    Implement authentication functionality: add login form, user authentication logic, and middleware for protected routes; update dependencies and configurations.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth';
 import { date, z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -101,4 +104,24 @@ export async function updateInvoice(
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
